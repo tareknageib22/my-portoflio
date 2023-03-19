472d57c407208de86b4b949274152ad346f3644d
Tarek Nageib <101251579+tareknageib22@users.noreply.github.com>
2023-03-19 05:33:39 +0200

    work in html

---
+++ locoscroll.js
@@ -0,0 +1,17 @@
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".locomotive-scroll"),
+  smooth: true,
+  smartphone: {
+    smooth: true,
+  },
+  tablet: {
+    smooth: true,
+  },
+  smoothMobile: 1,
+  multiplier: 1.0,
+});
+
+// Wait 2 seconds then calculate the new page height
+setTimeout(() => {
+  locoScroll.update();
+}, 2000);
