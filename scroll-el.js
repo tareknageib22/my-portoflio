731da29202927e31bed6d6303848c9abd0ce93a9
Tarek Nageib <101251579+tareknageib22@users.noreply.github.com>
2023-03-19 16:30:27 +0200

    sdsd

--- scroll-el.js
+++ scroll-el.js
@@ -1,12 +1,9 @@
 const lenis = new Lenis();
-
 lenis.on("scroll", (e) => {
   console.log(e);
 });
-
 function raf(time) {
   lenis.raf(time);
   requestAnimationFrame(raf);
 }
-
 requestAnimationFrame(raf);
