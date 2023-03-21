60354cc7bc77db1b7c8eed5a9d5ce7ce20108463
Tarek Nageib <tarek.nageib303@gmail.com>
2023-03-20 21:39:51 -0700

    d

--- darkmode.js
+++ darkmode.js
@@ -1,13 +1,16 @@
-const toggleButton = document.querySelector(".button.is-secondary");
+const sunIcon = document.querySelector(".sun-icon");
+const moonIcon = document.querySelector(".moon-icon");
 const body = document.querySelector("body");
+
 
 // Check if 'dark-mode' class was previously active
 if (localStorage.getItem("isDarkMode") === "true") {
   body.classList.add("background-color-black");
 }
 
-toggleButton.addEventListener("click", () => {
-  body.classList.toggle("background-color-black");
+
+moonIcon.addEventListener("click", () => {
+  body.classList.add("background-color-black");
   localStorage.setItem(
     "isDarkMode",
     body.classList.contains("background-color-black")
--- scroll-el.js
+++ scroll-el.js
@@ -1,9 +1,13 @@
+
 const lenis = new Lenis();
 lenis.on("scroll", (e) => {
   console.log(e);
 });
+
+
 function raf(time) {
   lenis.raf(time);
   requestAnimationFrame(raf);
+  
 }
 requestAnimationFrame(raf);
