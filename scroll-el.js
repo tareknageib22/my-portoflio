90dc6c72878d51008ef3e41a90c81698b0165670
Tarek Nageib <tarek.nageib303@gmail.com>
2023-03-20 21:58:16 -0700

    f

--- scroll-el.js
+++ scroll-el.js
@@ -3,11 +3,10 @@ const lenis = new Lenis();
 lenis.on("scroll", (e) => {
   console.log(e);
 });
-
-
 function raf(time) {
   lenis.raf(time);
   requestAnimationFrame(raf);
-  
 }
 requestAnimationFrame(raf);
+
+gsap.to("[target]", {x: 100, duration: 1});
