75d449bff4a10f4b95422542cccf2791150ffb82
Tarek Nageib <tarek.nageib303@gmail.com>
2023-03-21 20:20:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ if (!document.querySelector("html").classList.contains('w-editor')){
   const lenis = new Lenis({
     duration: 1.2,
     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
-    direction: 'vertical', // vertical, horizontal
-    gestureDirection: 'vertical', // vertical, horizontal, both
-    smooth: true,
+    orientation: 'vertical', // vertical, horizontal
+    ggestureOrientation: 'vertical', // vertical, horizontal, both
+    smoothWheel: true,
     smoothTouch: true,
     touchMultiplier: 2,
     infinite: false,
