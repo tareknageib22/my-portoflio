ce0f9864dc38387e260e8e9bc74e434a23285d9b
Tarek Nageib <tarek.nageib303@gmail.com>
2023-03-21 20:43:59 -0700

    Update smoothy2.js

--- smoothy2.js
+++ smoothy2.js
@@ -16,6 +16,16 @@ if (!document.querySelector("html").classList.contains('.page-wrapper')){
   lenis.on('scroll', ({ scroll, limit, velocity, direction, progress }) => {
     console.log({ scroll, limit, velocity, direction, progress })
   })*/
+  const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+    orientation: 'vertical', // vertical, horizontal
+    gestureOrientation: 'vertical', // vertical, horizontal, both
+    smoothWheel: true,
+    smoothTouch: false,
+    touchMultiplier: 2,
+    infinite: false,
+  })
   lenis.on('scroll', (e) => {
     console.log(e)
   })
