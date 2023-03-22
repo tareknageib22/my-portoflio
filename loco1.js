f5ac1e7ee8bb67e96c63a656ecdd07c42263ced5
Tarek Nageib <tarek.nageib303@gmail.com>
2023-03-21 22:27:21 -0700

    s

--- loco1.js
+++ loco1.js
@@ -1,9 +1,15 @@
-
 const locoScroll = new LocomotiveScroll({
   el: document.querySelector(".locomotive-scroll"),
   smooth: true,
+  smartphone: {
+      smooth: true
+  },
+  tablet: {
+      smooth: true
+  },
+	smoothMobile: 1,
   multiplier: 1.0,
-}); 
+});
 
 // Wait 2 seconds then calculate the new page height
 setTimeout(() => {  
