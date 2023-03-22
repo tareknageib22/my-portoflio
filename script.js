180d76028f07c8068c0155e5f52ca5ea22b58f58
Tarek Nageib <tarek.nageib303@gmail.com>
2023-03-21 20:07:02 -0700

    ff

---
+++ script.js
@@ -0,0 +1,37 @@
+
+//first basic code snippet
+if (!document.querySelector("html").classList.contains('w-editor')){
+  const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+    direction: 'vertical', // vertical, horizontal
+    gestureDirection: 'vertical', // vertical, horizontal, both
+    smooth: true,
+    smoothTouch: true,
+    touchMultiplier: 2,
+    infinite: false,
+  })}
+  
+    // Get scroll value. This is just for testing purposes. Delete this if you're not using the scroll value for anything.
+  lenis.on('scroll', ({ scroll, limit, velocity, direction, progress }) => {
+    console.log({ scroll, limit, velocity, direction, progress })
+  })
+
+  function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+  }
+
+  
+
+//here is gsap
+gsap.from('[tn-element-attribute="target"]', {
+  x: -200,
+  scale:1.5,
+  opacity:0,
+  duration: 0.5,
+  stagger: 0.3,
+  yoyo: true,
+  repeat:20,
+ 
+})
